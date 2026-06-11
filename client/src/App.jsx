2c4d351f7a97d4b47d2f8c7a64d787065c70312c
davidcarton <david.carton@gmail.com>
2026-06-11 20:20:46 +0200

    ajuste de dashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,4 @@
-import { HashRouter, Routes, Route } from 'react-router-dom'
-import LicitacionesPublicas from './pages/LicitacionesPublicas.jsx'
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Dashboard from './pages/Dashboard.jsx'
 import MisLicitaciones from './pages/MisLicitaciones.jsx'
 import Preferencias from './pages/Preferencias.jsx'
@@ -9,7 +8,7 @@ export default function App() {
   return (
     <HashRouter>
       <Routes>
-        <Route path="/" element={<LicitacionesPublicas />} />
+        <Route path="/" element={<Navigate to="/dashboard" replace />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/dashboard/licitaciones" element={<MisLicitaciones />} />
         <Route path="/dashboard/preferencias" element={<Preferencias />} />
